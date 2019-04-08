64c39d8285d4c6500374bf102c8595dc9718c069
ulitin.i <ulitin.i@milandr.ru>
2019-04-08 21:35:30 +0300

    Allowed CORS.

---
+++ src/main/java/ru/milandr/courses/miptshop/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package ru.milandr.courses.miptshop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    /**
+     * Enables Cross-Origin Resource Sharing (CORS)
+     * More info: http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cors.html
+     *
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
+
