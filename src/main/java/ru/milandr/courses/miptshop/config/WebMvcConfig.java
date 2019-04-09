a97adbd75596d5f0a7d931ce692f47cf6da352ca
ulitin.i <ulitin.i@milandr.ru>
2019-04-09 23:07:53 +0300

    Fixed an 404 on accessing swagger-ui with custom WebMvcConfigurerAdapter.

--- src/main/java/ru/milandr/courses/miptshop/config/WebMvcConfig.java
+++ src/main/java/ru/milandr/courses/miptshop/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package ru.milandr.courses.miptshop.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -17,6 +18,22 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+    /**
+     * Configures path to static resources.
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
     }
 }
 
